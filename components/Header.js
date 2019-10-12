aa0f7696366f6bfe739ad3c8d5d54ff5cc92c0ec
徐辉波 <991923257@qq.com>
2019-10-12 21:56:45 +0800

    first commit

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../static/style/components/header.css'
+
+import {Row,Col, Menu, Icon} from 'antd'
+
+export default () => (
+  <div className="header">
+    <Row type="flex" justify="center">
+        <Col  xs={24} sm={24} md={10} lg={10} xl={10}>
+            <span className="header-logo">星空守候</span>
+            <span className="header-txt">有志者,事竟成;有心人,天不负</span>
+        </Col>
+       
+        <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
+            <Menu  mode="horizontal">
+                <Menu.Item key="home">
+                    <Icon type="home" />
+                    首页
+                </Menu.Item>
+                <Menu.Item key="video">
+                    <Icon type="youtube" />
+                    视频
+                </Menu.Item>
+                <Menu.Item key="life">
+                    <Icon type="smile" />
+                    生活
+                </Menu.Item>
+            </Menu>
+        </Col>
+    </Row>
+ </div>
+)
